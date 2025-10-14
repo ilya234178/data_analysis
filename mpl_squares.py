6ed2becd9e5e042bdcaa54870d3415a127c1faeb
Ilya2311 <veruhanov00@mail.ru>
2025-10-14 10:20:01 +0300

    scatter_squares.py

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,6 +1,16 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
+plt.style.use('bmh')
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(input_values, squares, linewidth=3)
+
+#задание заголовка диаграммы и меток осей
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+#задание размера шрифта деоений на осях
+ax.tick_params(labelsize=14)
 plt.show()
